c23e1f583f4f2ff803c2f8a2c35a2d1de453ca42
Kate444ka <ekaterinaskarzinec@gmail.com>
2022-12-03 00:11:38 +0200

    Initial commit

---
+++ src/List.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const List = ({people}) => {
+  return (
+    <>
+      {people.map((person)=> {
+        return <article key={person.id} className="person">
+          <img src={person.image} alt={person.name}/>
+          <div>
+            <h3>{person.name}</h3>
+            <p>{person.age} years</p>
+          </div>
+        </article>
+      })}
+    </>
+  );
+};
+
+export default List;
